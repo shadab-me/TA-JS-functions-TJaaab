da813f10c5f81f9cd4cb65cf88bfcc1337c2e3b1
Shadab Ali <shadabali604@gmail.com>
2020-08-17 22:18:58 +0530

    added: function with parameter

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -1,7 +1,10 @@
 /*
 1. Create a function named `sayHello` that that accepts a parameter `name` and alert `Hello [name]!`.
 */
-
+function sayHello(name){
+  alert(`Hello ${name}`)
+}
+sayHello()
 /*
 2. Create a function named `getFullName` that accepts two parameter `firstName` and `lastName` and returns
 fullName.
@@ -10,6 +13,10 @@ Example:
   getFullName("John", "Snow"); // "John Snow"
   getFullName("Nelson", "Mandela"); // "Nelson Mandela"
 */
+
+function getFullName(firstName, lastName){
+  return firstName+" "+lastName;
+}
 
 /*
 3. Create a function named `addTwoNumbers` that accepts two numbers i.e `firstNum` and `secondNum` and returns
@@ -21,6 +28,13 @@ addTwoNumbers(20, 32); // 32
 addTwoNumbers(10, "100"); // Alert Enter Valid Input
 */
 
+function addTwoNumbers(firstNum, secondNum){
+  if(typeof firstNum == 'number' && typeof secondNum){
+    return firstNum+secondNum;
+  }else{
+    alert('Enter a valid Input')
+  }
+}
 /*
 4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
 can only be one of these `add, sub, mul, div`. Based on the operator return the value. If the `numA` or `numB`
@@ -33,6 +47,40 @@ calc(20, 10, 'sub'); // 10
 calc(20, 10, 'mul'); // 200
 */
 
+function calc(numA, numB, operation){
+  if(operation == 'add'){
+    if(typeof numA == 'number' && typeof numB == 'number'){
+      return numA + numB
+    }
+    else{
+      alert('Enter a valid input')
+    }
+  }
+  else if(operation == 'sub'){
+    if(typeof numA == 'number' && typeof numB == 'number'){
+      return numA - numB;
+    }else{
+      alert('Enter a valid input')
+    }
+  }
+  else if(operation == 'mul'){
+    if(typeof numA == 'number' && typeof numB == 'number'){
+      return numA * numB
+    }
+    else{
+      alert('Enter a valid input')
+    }
+  }
+  else if(operation == 'div'){
+    if(typeof numA == 'number' && typeof numB == 'number'){
+    return numA / numB
+    }
+    else{
+      alert('Enter a valid input')
+    }
+  }
+
+}
 
 /*
 5. Create a function named `isLeapYear` that accepts a number data type and return `true` or `false` based
@@ -41,8 +89,20 @@ on if the year id leap year or not.
 isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
-
+function leapYear(year)
+{
+  return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+}
 
 /*
 6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
 */
+
+function getFactorial(n){
+  let factorial = 1;
+  for(let i = n; i > 0; i--){
+    factorial *= i
+  }
+  return factorial
+}
+getFactorial(5)
